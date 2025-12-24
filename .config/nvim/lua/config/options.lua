0158c84abdbc0f566fe945b7e6f48c57654f7e8c
Nabin Thapa <erewhonaden23@gmail.com>
2025-12-24 19:49:57 +0545

    update nvim to lazyvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,3 @@
-vim.opt.clipboard = "unnamedplus"
-vim.opt.shiftwidth = 4
-
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.tabstop = 2
-vim.cmd.colorscheme("tokyonight")
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
